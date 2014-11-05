59a9b539f36620b4fb52a61631d482e904eb1cdd
Patrick Baselier <patrick@kabisa.nl>
2014-11-05 18:33:32 +0100

    Update user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@ role :db,  %w{patrickbaselier.nl}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'patrickbaselier.nl', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'patrickbaselier.nl', user: 'passenger', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
